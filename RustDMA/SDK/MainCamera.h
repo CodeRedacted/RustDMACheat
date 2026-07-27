bb4dd1eeddeaaa4b913b6fbc05f63269157b142b
CodeRedacted <167279001+CodeRedacted@users.noreply.github.com>
2026-07-27 16:49:28 -0600

    Change Camera static field value in MainCamera.h
    Updated the Camera static field value for MainCamera.

--- RustDMA/SDK/MainCamera.h
+++ RustDMA/SDK/MainCamera.h
@@ -10,7 +10,7 @@ class MainCamera
 	uint64_t Class = 0xFCB7488;
 	//Dump.cs / DummyDLL
 	uint64_t StaticField = 0xB8;// Static Padding To Access Static Fields
-	uint64_t Camera = 0x0;// public static Camera mainCamera
+	uint64_t Camera = 0xB8;// public static Camera mainCamera
 
 	uint64_t CameraGameObject = 0x8; //You can't find these in il2cpp dumps, instead use IDA or CE
 	uint64_t ViewMatrixOffset = 0x2FC; //You can't find these in il2cpp dumps, instead use IDA or CE
