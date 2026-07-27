4b5d63f89fdbe33b1a2e3b8fb9b36bffd3bdaa63
CodeRedacted <167279001+CodeRedacted@users.noreply.github.com>
2026-07-27 16:41:58 -0600

    uPDATE OFFSETS

--- RustDMA/SDK/TODSky.h
+++ RustDMA/SDK/TODSky.h
@@ -6,15 +6,15 @@ class TODSky // class TOD_Sky
       "Name": "TOD_Sky_TypeInfo",
       "Signature": "TOD_Sky_c*"
 	*/
-	uint64_t Class = 0x3C303D8;
+	uint64_t Class = 0xd77d370;
 	uint64_t StaticField = 0xB8;
 	uint64_t NightParameters = 0x60; // public TOD_NightParameters Night;
 	uint64_t DayParameters = 0x58; // 	public TOD_DayParameters Day;
 	uint64_t Instance = 0;
-	uint64_t AmbientMultiplierDay = 0x50; // TOD_NightParameters -> public float AmbientMultiplier;
-	uint64_t AmbientMultiplierNight = 0x50; // TOD_NightParameters -> public float AmbientMultiplier;
-	uint64_t LightIntensityDay = 0x48; // TOD_NightParameters -> public float LightIntensity;
-	uint64_t LightIntensityNight = 0x48; // TOD_NightParameters -> public float LightIntensity;
+	uint64_t AmbientMultiplierDay = 0x58; // TOD_NightParameters -> public float AmbientMultiplier;
+	uint64_t AmbientMultiplierNight = 0x54; // TOD_NightParameters -> public float AmbientMultiplier;
+	uint64_t LightIntensityDay = 0x4c; // TOD_NightParameters -> public float LightIntensity;
+	uint64_t LightIntensityNight = 0x54; // TOD_NightParameters -> public float LightIntensity;
 public:
 	TODSky();
 	void WriteNightLightIntensity(VMMDLL_SCATTER_HANDLE handle, float value);
