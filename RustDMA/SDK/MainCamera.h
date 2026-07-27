b662fd6edab055000414254b778e02a6c1b53b06
CodeRedacted <167279001+CodeRedacted@users.noreply.github.com>
2026-07-26 18:40:41 -0600

    Updated Offsets

--- RustDMA/SDK/MainCamera.h
+++ RustDMA/SDK/MainCamera.h
@@ -12,8 +12,8 @@ class MainCamera
 	uint64_t StaticField = 0xB8;// Static Padding To Access Static Fields
 	uint64_t Camera = 0x0;// public static Camera mainCamera
 
-	uint64_t CameraGameObject = 0x10; //You can't find these in il2cpp dumps, instead use IDA or CE
-	uint64_t ViewMatrixOffset = 0x30C; //You can't find these in il2cpp dumps, instead use IDA or CE
+	uint64_t CameraGameObject = 0x444; //You can't find these in il2cpp dumps, instead use IDA or CE
+	uint64_t ViewMatrixOffset = 0x2FC; //You can't find these in il2cpp dumps, instead use IDA or CE
 public:
 	MainCamera();
 	ViewMatrix GetViewMatrix();
